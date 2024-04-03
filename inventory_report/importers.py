e5197d8b3a0250f9e04ce6e7bba0f11bd2a51eb0
Stephan <cicchelli.stephan@gmail.com>
2024-04-03 11:32:10 -0300

    Req4 e 8 - JsonImporter / CsvImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,3 +1,5 @@
+import csv
+import json
 from abc import ABC, abstractmethod
 from typing import Dict, Type
 
@@ -13,12 +15,42 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path, "r") as file:
+            json_data = json.load(file)
+            products = []
+            for item in json_data:
+                product = Product(
+                    item["id"],
+                    item["product_name"],
+                    item["company_name"],
+                    item["manufacturing_date"],
+                    item["expiration_date"],
+                    item["serial_number"],
+                    item["storage_instructions"],
+                )
+                products.append(product)
+            return products
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> list[Product]:
+        with open(self.path, "r") as file:
+            csv_data = csv.DictReader(file)
+            products = []
+            for item in csv_data:
+                product = Product(
+                    item["id"],
+                    item["product_name"],
+                    item["company_name"],
+                    item["manufacturing_date"],
+                    item["expiration_date"],
+                    item["serial_number"],
+                    item["storage_instructions"],
+                )
+                products.append(product)
+            return products
 
 
 # Não altere a variável abaixo
