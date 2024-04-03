369e15764588dec3d39f2fc1655e141c170a4bdf
Stephan <cicchelli.stephan@gmail.com>
2024-04-03 08:41:32 -0300

    Req1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,18 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product_data = {
+        "id": "1",
+        "product_name": "Mouse Gamer",
+        "company_name": "Logitech",
+        "manufacturing_date": "10/01/2024",
+        "expiration_date": "10/01/2033",
+        "serial_number": "123456789",
+        "storage_instructions": "frágil",
+    }
+
+    product = Product(**product_data)
+
+    for key, value in product_data.items():
+        assert getattr(product, key) == value
